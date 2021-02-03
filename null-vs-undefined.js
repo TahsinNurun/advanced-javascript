345d5338ee807f9a6c341e112c1252e691c46658
Tahsin Nurun <nuruntahsin@gmail.com>
2021-02-03 21:58:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+// let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1,num2);
+// }
+// const result = add(13);
+
+// const premik = {name:"smart", phone: 080980};
+// console.log(primik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 3, 8];
+console.log(ages[11]);
